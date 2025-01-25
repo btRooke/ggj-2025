68311612f1db32796449f71336918fcbce612b05
billy <rookebilly@gmail.com>
2025-01-25 11:19:08 +0000

    Add Tim's mypy checker

--- src/ggj/main.py
+++ src/ggj/main.py
@@ -1,1 +1,16 @@
-print("hello world")
+import sys
+from pathlib import Path
+
+from mypy import api
+
+PACKAGE_ROOT = Path(__file__).parent.resolve()
+
+def _run_mypy():
+    out, err, code = api.run([str(PACKAGE_ROOT)])
+    print(out, file=sys.stdout, end="")
+    print(err, file=sys.stderr, end="")
+    if code != 0:
+        exit(code)
+
+if __name__ == "__main__":
+    _run_mypy()
