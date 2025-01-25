9bb875402ed63d3392076e4680a822d11081318a
billy <rookebilly@gmail.com>
2025-01-25 23:44:38 +0000

    Fix circular import (temporary eek)

--- src/ggj/world/terrain.py
+++ src/ggj/world/terrain.py
@@ -2,9 +2,11 @@ from typing_extensions import Dict, Callable
 from ..drawing import shape as s
 from .manager import WorldManager
 from .gameobject import GameObject
-from .player import Player
+
+# from .player import Player
 import random
 import time
+
 
 class Grass:
     def __init__(self, x: int, y: int):
@@ -17,7 +19,7 @@ class Grass:
     def draw(self):
         assert WorldManager.screen
         x, y = self.pos
-        s.world_char(WorldManager.screen, x, y, ';', self.colour)
+        s.world_char(WorldManager.screen, x, y, ";", self.colour)
 
     def get_pos(self) -> tuple[int, int]:
         return self.pos[0], self.pos[1]
@@ -27,6 +29,7 @@ class Grass:
 
     def impassable(self) -> bool:
         return False
+
 
 class Boundary:
     def __init__(self, x: int, y: int):
@@ -38,7 +41,7 @@ class Boundary:
     def draw(self):
         assert WorldManager.screen
         x, y = self.pos
-        s.world_char(WorldManager.screen, x, y, '♠', s.DARK_GREEN)
+        s.world_char(WorldManager.screen, x, y, "♠", s.DARK_GREEN)
 
     def get_pos(self) -> tuple[int, int]:
         return self.pos[0], self.pos[1]
@@ -48,6 +51,7 @@ class Boundary:
 
     def impassable(self) -> bool:
         return True
+
 
 class Hole:
     def __init__(self, x: int, y: int):
@@ -59,7 +63,7 @@ class Hole:
     def draw(self):
         assert WorldManager.screen
         x, y = self.pos
-        s.world_char(WorldManager.screen, x, y, 'O', s.MAROON)
+        s.world_char(WorldManager.screen, x, y, "O", s.MAROON)
 
     def get_pos(self) -> tuple[int, int]:
         return self.pos[0], self.pos[1]
@@ -70,7 +74,9 @@ class Hole:
     def impassable(self) -> bool:
         return False
 
+
 SPILL_INTERVAL = 2
+
 
 class Water:
     def __init__(self, x: int, y: int):
@@ -93,17 +99,17 @@ class Water:
             (1, 0),
             (-1, 1),
             (0, 1),
-            (1, 1)
-          ]
+            (1, 1),
+        ]
 
         for vec_x, vec_y in surrounding_vector:
             n_x, n_y = pos_x + vec_x, pos_y + vec_y
             objs = WorldManager.get_objects(n_x, n_y)
 
             is_hole = any(filter(lambda o: isinstance(o, Hole), objs))
-            is_player = any(filter(lambda o: isinstance(o, Player), objs))
+            # is_player = any(filter(lambda o: isinstance(o, Player), objs)) TODO eek fix circular import
 
-            if is_hole and not is_player:
+            if is_hole:  #  and not is_player:
                 WorldManager.clear_cell(n_x, n_y)
                 WorldManager.add_object(Water(n_x, n_y))
 
@@ -115,13 +121,14 @@ class Water:
     def draw(self):
         assert WorldManager.screen
         x, y = self.pos
-        s.world_char(WorldManager.screen, x, y, '~', s.DEEP_BLUE)
+        s.world_char(WorldManager.screen, x, y, "~", s.DEEP_BLUE)
 
     def zindex(self) -> int:
         return 0
 
     def impassable(self) -> bool:
         return True
+
 
 class Soil:
     def __init__(self, x: int, y: int):
@@ -133,7 +140,7 @@ class Soil:
     def draw(self):
         assert WorldManager.screen
         x, y = self.pos
-        s.world_char(WorldManager.screen, x, y, '!', s.LIGHT_BROWN)
+        s.world_char(WorldManager.screen, x, y, "!", s.LIGHT_BROWN)
 
     def get_pos(self) -> tuple[int, int]:
         return self.pos[0], self.pos[1]
@@ -143,6 +150,7 @@ class Soil:
 
     def impassable(self) -> bool:
         return True
+
 
 class PlantedSoil:
     def __init__(self, x: int, y: int):
@@ -154,7 +162,7 @@ class PlantedSoil:
     def draw(self):
         assert WorldManager.screen
         x, y = self.pos
-        s.world_char(WorldManager.screen, x, y, '`', s.LIGHT_YELLOW)
+        s.world_char(WorldManager.screen, x, y, "`", s.LIGHT_YELLOW)
 
     def get_pos(self) -> tuple[int, int]:
         return self.pos[0], self.pos[1]
@@ -164,6 +172,7 @@ class PlantedSoil:
 
     def impassable(self) -> bool:
         return True
+
 
 class Wheat:
     def __init__(self, x: int, y: int):
@@ -175,7 +184,7 @@ class Wheat:
     def draw(self):
         assert WorldManager.screen
         x, y = self.pos
-        s.world_char(WorldManager.screen, x, y, '$', s.GOLDEN)
+        s.world_char(WorldManager.screen, x, y, "$", s.GOLDEN)
 
     def get_pos(self) -> tuple[int, int]:
         return self.pos[0], self.pos[1]
@@ -186,22 +195,23 @@ class Wheat:
     def impassable(self) -> bool:
         return True
 
+
 class TerrainFactory:
     @staticmethod
     def create_terrain(world: list[list[str]]):
         terrain_map: Dict[str, Callable[[int, int], GameObject]] = {
-            ';': Grass,
-            '♠': Boundary,
-            '~': Water,
-            '!': Soil,
-            '^': PlantedSoil,
-            '$': Wheat,
+            ";": Grass,
+            "♠": Boundary,
+            "~": Water,
+            "!": Soil,
+            "^": PlantedSoil,
+            "$": Wheat,
         }
 
         for y, row in enumerate(world):
             for x, cell in enumerate(row):
                 if cell not in terrain_map:
-                    raise Exception ("terrain tile does not exist")
+                    raise Exception("terrain tile does not exist")
 
                 obj: GameObject = terrain_map[cell](x, y)
                 WorldManager.add_object(obj)
