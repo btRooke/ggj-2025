b006cd7aab6cef03af423e0949dc9d55078074cd
billy <rookebilly@gmail.com>
2025-01-25 11:27:45 +0000

    Add comment for checker

--- src/ggj/main.py
+++ src/ggj/main.py
@@ -5,12 +5,20 @@ from mypy import api
 
 PACKAGE_ROOT = Path(__file__).parent.resolve()
 
-def _run_mypy():
+
+def _run_mypy() -> None:
+    """
+    Run Tim's mypy checker on the package before the game starts.
+
+    Program exits if error code is bad.
+    """
     out, err, code = api.run([str(PACKAGE_ROOT)])
     print(out, file=sys.stdout, end="")
     print(err, file=sys.stderr, end="")
     if code != 0:
         exit(code)
 
+
 if __name__ == "__main__":
     _run_mypy()
+    print("Game code here")
